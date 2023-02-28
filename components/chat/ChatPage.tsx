d771e45e5756b10c1edd4374dd5e027acf949957
alex chika <mcluckey1@gmail.com>
2023-02-28 14:11:55 +0100

    reverted setheight hook aproach

--- components/chat/ChatPage.tsx
+++ components/chat/ChatPage.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import SetHeight from "../../lib/hooks/setHeight";
 import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 import { orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
@@ -40,7 +39,6 @@ const ChatPage = ({ chatId }: Props) => {
     return isNew;
   }
 
-  SetHeight(chatPageRef);
   useEffect(() => {
     if (chatPageRef.current) {
       chatPageRef.current.scrollTo(0, Number(chatPageRef.current.scrollHeight));
