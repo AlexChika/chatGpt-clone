6fc6a734aab1f218b79947b436c73b6eb47c9ab8
alex chika <mcluckey1@gmail.com>
2023-03-13 03:22:36 +0100

    updates and fixes

--- components/layout/LoginPage.tsx
+++ components/layout/LoginPage.tsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
 
       <button
         onClick={() => signIn(`google`)}
-        className="bg-neutral-600 rounded text-white font-bold text-xl animate-bounce p-3 mt-10 hover:bg-[#11a37f] transition-colors duration-150 active:bg-neutral-800"
+        className="bg-neutral-600 rounded text-white font-bold text-xl animate-bounce p-3 mt-10 hover:bg-[#11a37f] transition-colors duration-150"
       >
         Sign in to use ChatGPT
       </button>
